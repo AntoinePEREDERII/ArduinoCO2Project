0b496634c9a7a335abf030d0e9cca748beab1a41
Antoine_PEREDERII <149400832+AntoinePEREDERII@users.noreply.github.com>
2023-12-10 11:38:48 +0100

    Create secrets.h

---
+++ src/espCO2thingspeak/secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "NomDuReseauWiFi"    // Remplacer NomDuReseauWiFi par le nom de votre réseau Wi-Fi
+#define SECRET_PASS "MotDePasseWiFi"      // Remplacer MotDePasseWiFi par le mot de passe de votre réseau Wi-Fi
+
+#define SECRET_CH_ID 000000               // Remplacer 000000 par le numéro de votre canal ThingSpeak
+#define SECRET_WRITE_APIKEY "VotreCleAPI" // Remplacer VotreCleAPI par votre clé API d'écriture ThingSpeak
